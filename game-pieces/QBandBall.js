52957fb2f31e73b1b2c8a7c9d9754f18686e7143
jimjea <jimjea@gmail.com>
2014-10-08 14:56:26 -0700

    more sizing

--- game-pieces/QBandBall.js
+++ game-pieces/QBandBall.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
         // adjusts the size of the ball as it travels)
         step: function(ev) { 
           increment--;
-          if (increment > count/1.6) {
+          if (increment > count/1.5) { // mobile: 1.6, web: 2
             ballHeight += .5;
             ballWidth += .45;
           } else {
